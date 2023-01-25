ecc88248ab839f93ddef8a305eb9dd43aa707a97
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2023-01-25 02:06:21 -0500

    agregando gitflow_release

---
+++ makefiles/gitflow.mk
@@ -0,0 +1,2 @@
+gitflow_release:
+	@export GIT_HOME=${GIT_HOME} && ${GIT_HOME}/scripts/gitflow.sh function_gitflow_release
---
+++ scripts/gitflow.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export AWS_DEFAULT_REGION="us-east-1"
+
+function_loading () {
+  source "${GIT_HOME}"/scripts/build.sh
+  source "${GIT_HOME}"/scripts/env.sh
+  source "${GIT_HOME}"/scripts/gitflow.sh
+  source "${GIT_HOME}"/scripts/release.sh
+}
+
+
+function_gitflow_release () {
+  # cargando scripts
+  function_loading
+  function_env_global
+  function_env_docker
+
+  # ejecutando proceso
+  docker run -e GITHUB_TOKEN=${GIT_TOKEN} -e GITHUB_REPO=$(echo "${GITHUB_REPOSITORY}" | cut -d "/" -f2) titan-release:latest
+}
+
+"$@"
