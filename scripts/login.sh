6c44a2dc0fde1ed5dc674e36382c98b4cbe095fa
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2023-03-05 01:18:11 -0500

    agregando awspec

---
+++ scripts/login.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export AWS_DEFAULT_REGION="us-east-1"
+
+function_loading () {
+  source "${GIT_HOME}"/scripts/build.sh
+  source "${GIT_HOME}"/scripts/env.sh
+  source "${GIT_HOME}"/scripts/gitflow.sh
+  source "${GIT_HOME}"/scripts/release.sh
+  source "${GIT_HOME}"/scripts/terraform.sh
+  source "${GIT_HOME}"/scripts/test.sh
+}
+
+function_login_aws () {
+  # generando credenciales temporales
+  AWS_CREDENTIALS=$(aws sts get-session-token --duration-seconds 3600 --output json --region "${AWS_DEFAULT_REGION}")
+
+  # parseando credenciales
+  AWS_ACCESS_KEY_ID=$(echo "${AWS_CREDENTIALS}" | jq -r .Credentials.AccessKeyId)
+  AWS_SECRET_ACCESS_KEY=$(echo "${AWS_CREDENTIALS}" | jq -r .Credentials.SecretAccessKey)
+  AWS_SESSION_TOKEN=$(echo "${AWS_CREDENTIALS}" | jq -r .Credentials.SessionToken)
+  export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}"
+  export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}"
+  export AWS_SESSION_TOKEN="${AWS_SESSION_TOKEN}"
+}
+
+"$@"
---
+++ scripts/test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+export AWS_DEFAULT_REGION="us-east-1"
+
+function_loading () {
+  source "${GIT_HOME}"/scripts/build.sh
+  source "${GIT_HOME}"/scripts/env.sh
+  source "${GIT_HOME}"/scripts/gitflow.sh
+  source "${GIT_HOME}"/scripts/release.sh
+  source "${GIT_HOME}"/scripts/terraform.sh
+}
+
+function_test_awspec () {
+  # cargando scripts
+  function_loading
+  function_env_global
+  function_env_docker
+  function_login_aws
+
+  # ejecutando pruebas
+  docker run --rm -u "${DOCKER_UID}":"${DOCKER_GID}" -v "${PWD}"/passwd:/etc/passwd:ro -v "${PWD}"/terraformrc:/home/terraformrc:ro -v "${PWD}":/app \
+    -e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
+    -e AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
+    -e AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION}" \
+  titan-awspec:latest
+}
+
+"$@"
