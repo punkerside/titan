199ed8ce468b321666a44c3edfc2dbd8ccd5b5aa
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2023-03-05 15:49:18 -0500

    corrigiendo test

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,6 +6,7 @@ function_loading () {
   source "${GIT_HOME}"/scripts/build.sh
   source "${GIT_HOME}"/scripts/env.sh
   source "${GIT_HOME}"/scripts/gitflow.sh
+  source "${GIT_HOME}"/scripts/login.sh
   source "${GIT_HOME}"/scripts/release.sh
   source "${GIT_HOME}"/scripts/terraform.sh
 }
@@ -18,7 +19,7 @@ function_test_awspec () {
   function_login_aws
 
   # ejecutando pruebas
-  docker run --rm -u "${DOCKER_UID}":"${DOCKER_GID}" -v "${PWD}"/passwd:/etc/passwd:ro -v "${PWD}"/terraformrc:/home/terraformrc:ro -v "${PWD}":/app \
+  docker run --rm -u "${DOCKER_UID}":"${DOCKER_GID}" -v "${PWD}"/passwd:/etc/passwd:ro -v "${PWD}":/app \
     -e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
     -e AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
     -e AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION}" \
