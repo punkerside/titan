03c20fd53c10ed7d374bd90adcc06bccbbc3347e
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2023-03-06 23:09:56 -0500

    corrigiendo token

--- scripts/test.sh
+++ scripts/test.sh
@@ -33,7 +33,6 @@ function_test_awspec () {
   docker run --rm -u "${DOCKER_UID}":"${DOCKER_GID}" -v "${PWD}"/passwd:/etc/passwd:ro -v "${PWD}":/app \
     -e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
     -e AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
-    -e AWS_SESSION_TOKEN="${AWS_SESSION_TOKEN}" \
     -e AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION}" \
   punkerside/titan-awspec:latest
 }
