907124ac7ef7454b677f9539f172df85cd09982e
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2023-03-20 21:47:11 -0500

    release

--- scripts/gitflow.sh
+++ scripts/gitflow.sh
@@ -18,7 +18,7 @@ function_gitflow_release () {
   function_env_docker
 
   # ejecutando proceso
-  docker run -e GITHUB_TOKEN=${GIT_TOKEN} -e GITHUB_REPO=$(echo "${GITHUB_REPOSITORY}" | cut -d "/" -f2) punkerside/titan-gh:latest
+  docker run -e GITHUB_TOKEN=${GIT_TOKEN} -e GITHUB_REPO=$(echo "${GITHUB_REPOSITORY}" | cut -d "/" -f2) punkerside/titan-image-gh:latest
 }
 
 "$@"
