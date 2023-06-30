39dbbd7cee2d3518a81ef9bc96de6d137dc88ff7
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2023-06-30 00:48:49 -0500

    agregando lint

---
+++ makefiles/lint.mk
@@ -0,0 +1,2 @@
+lint_hadolint:
+	@export GIT_HOME=${GIT_HOME} && ${GIT_HOME}/scripts/lint.sh function_lint_hadolint
---
+++ scripts/lint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function_loading () {
+  source "${GIT_HOME}"/scripts/env.sh
+}
+
+function_lint_hadolint () {
+  # cargando scripts
+  function_loading
+
+  # cargando variables globales
+  function_env_global
+
+  # ejecutando proceso
+  docker run --rm -i hadolint/hadolint < Dockerfile
+}
+
+"$@"
