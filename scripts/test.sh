f045f9028d270e0c5db9b2dc7fdeb1f2df92b1b1
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2023-07-01 00:42:53 -0500

    corrigiendo precommit

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 function_loading () {
-  source "${GIT_HOME}"/scripts/build.sh
   source "${GIT_HOME}"/scripts/env.sh
-  source "${GIT_HOME}"/scripts/gitflow.sh
-  source "${GIT_HOME}"/scripts/login.sh
-  source "${GIT_HOME}"/scripts/release.sh
-  source "${GIT_HOME}"/scripts/terraform.sh
-  source "${GIT_HOME}"/scripts/test.sh
 }
 
 function_test_awspec () {
   # cargando scripts
   function_loading
+
+  # cargando variables globales
   function_env_global
+
+  # cargando variables docker
   function_env_docker
 
   if [ -z "${AWS_ACCESS_KEY_ID}" ]
@@ -33,7 +31,7 @@ function_test_awspec () {
     -e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
     -e AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
     -e AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION}" \
-  punkerside/titan-image-awspec:latest
+  punkerside/awspec:latest
 }
 
 function_test_precommit () {
@@ -43,7 +41,7 @@ function_test_precommit () {
   function_env_docker
 
   # ejecutando pruebas
-  docker run --rm -u "${DOCKER_UID}":"${DOCKER_GID}" -v "${PWD}"/passwd:/etc/passwd:ro -v "${PWD}":/app punkerside/titan-precommit:latest
+  docker run --rm -u "${DOCKER_UID}":"${DOCKER_GID}" -v "${PWD}"/passwd:/etc/passwd:ro -v "${PWD}":/app punkerside/precommit:latest
 }
 
 "$@"
